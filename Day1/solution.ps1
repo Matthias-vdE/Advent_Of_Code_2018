7340a5f9df3ddfba9eb1d367e1a3cb81582a1074
Matthias-vdE <matthias.vandenelsacker@gmail.com>
2018-12-02 16:10:25 +0100

    Day 1
    Solution for Day 1

--- Day1/solution.ps1
+++ Day1/solution.ps1
@@ -1,1 +1,29 @@
 # Solution for https://adventofcode.com/2018/day/1
+
+Function Part1 {
+    $counter = 0;
+    $array = Get-Content .\input.txt
+    for ($i = 0; $i -lt $array.Length; $i++) {
+        $counter += $array[$i]
+    } 
+    write-host $counter
+}
+
+Function Part2 {
+    $counter = 0;
+    $array = Get-Content .\input.txt
+    $setofvalues = New-Object System.Collections.Generic.HashSet[int]
+    while ($true) {
+        for ($i = 0; $i -lt $array.Length; $i++) {
+            $counter += $array[$i]
+            if (-not($setofvalues.Add($counter))){
+                write-host $counter
+                exit
+            }
+
+        } 
+    }
+}
+
+Part1
+Part2
