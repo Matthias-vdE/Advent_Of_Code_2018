16dc38cb0605a2f71690e6c8f09acdbc9d96c0be
Matthias-vdE <matthias.vandenelsacker@gmail.com>
2018-12-03 10:54:56 +0100

    Day 3
    Solution for Day 3 - Part 2

--- Day3/solution.ps1
+++ Day3/solution.ps1
@@ -27,6 +27,47 @@ Function Part1 {
     write-host $clashcounter
 }
 
+Function Part2 {
+    $fabric = New-Object 'object[,]' 1000, 1000
+    $clashcounter = 0
+    for ($i = 0; $i -lt 1000; $i++) {
+        for ($j = 0; $j -lt 1000; $j++) {
+            $fabric[$i, $j] = '.'
+        }
+    }
+    $input = Get-Content .\input.txt
+    for ($traverse = 0; $traverse -le 1; $traverse++) {
+        foreach ($line in $input) {
+            $tofill = InputParser $line
+            $id = ($line -split '@')[0]
+            $causedclash = $false
+            foreach ($filler in $tofill) {
+                $split = $filler -split ","
+                $x = [int]($split[0])
+                $y = [int]($split[1])
+                if ($fabric[$x, $y] -eq '.') {
+                    $fabric[$x, $y] = '#'
+                }
+                elseif ($fabric[$x, $y] -eq '#') {
+                    $fabric[$x, $y] = 'X'
+                    $clashcounter++
+                    if ($traverse -eq 0) {
+                        $causedclash = $true
+                    }
+                }
+                elseif ($fabric[$x, $y] -eq 'X') {
+                    $causedclash = $true
+                }
+            }
+            if ($traverse -eq 1) {
+                if ($causedclash -eq $false) {
+                    write-host "$id"
+                }
+            }
+        }
+    }
+}
+
 Function InputParser {
     param(
         [Parameter(Mandatory)]
@@ -34,7 +75,6 @@ Function InputParser {
         $inputstring
     )
     $split1 = $inputstring -split '@'
-    $id = $split1[0].Trim()
     $split2 = $split1[1] -split ":"
     $pos = $split2[0].Trim()
     $sqr = $split2[1].Trim()
@@ -56,8 +96,5 @@ Function InputParser {
     return $tofill
 }
 
-Function Part2 {
-
-}
-
 Part1
+Part2
