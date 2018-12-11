e8f1d871dbf3d2b74acd65c8d2f2b9895bcb3a4e
Matthias-vdE <matthias.vandenelsacker@gmail.com>
2018-12-11 16:46:01 +0100

    Day 11 - Part 1
    Fuck Part 2. Easiest shit ever but I can't figure it out.

--- Day11/solution.ps1
+++ Day11/solution.ps1
@@ -30,7 +30,7 @@ Function Part1 {
 
     foreach ($x in (0..297)) {
         foreach ($y in (0..297)) {
-            $sum = $grid[$x][$y] + $grid[$x+1][$y] + $grid[$x+2][$y] + $grid[$x][$y+1] + $grid[$x+1][$y+1] + $grid[$x+2][$y+1] + $grid[$x][$y+2] + $grid[$x+1][$y+2] + $grid[$x+2][$y+2]
+            $sum = $grid[$x][$y] + $grid[$x + 1][$y] + $grid[$x + 2][$y] + $grid[$x][$y + 1] + $grid[$x + 1][$y + 1] + $grid[$x + 2][$y + 1] + $grid[$x][$y + 2] + $grid[$x + 1][$y + 2] + $grid[$x + 2][$y + 2]
             if ($sum -gt $largestPower) {
                 $largestPower = $sum
                 $largestX = $x + 1
@@ -42,47 +42,33 @@ Function Part1 {
     write-host "$largestX,$largestY"
 }
 
-Function Part1 {
-    $grid = New-Object int[][] 300, 300
-    $gridSN = 5719 #This is the puzzle input
-    foreach ($x in (0..299)) {
-        foreach ($y in (0..299)) {
-            $xCoordinate = $x + 1
-            $yCoordinate = $y + 1
-            $rackID = $xCoordinate + 10
-            $powerLevel = $rackID * $yCoordinate
-            $powerLevel += $gridSN
-            $powerLevel *= $rackID
-            if ($powerLevel -lt 100) {
-                $hundreth = 0
-            }
-            else {
-                $string = [String]$powerlevel
-                $hundreth = $string.Substring($string.Length - 3, 1)
-                $hundreth = [int]$hundreth
-            }
-            $final = $hundreth - 5
-            $grid[$x][$y] = $final
-        }
-    }
-
-    $largestPower = -1
-    $largestX = -1
-    $largestY = -1
-
-    foreach ($x in (0..297)) {
-        foreach ($y in (0..297)) {
-            $sum = $grid[$x][$y] + $grid[$x+1][$y] + $grid[$x+2][$y] + $grid[$x][$y+1] + $grid[$x+1][$y+1] + $grid[$x+2][$y+1] + $grid[$x][$y+2] + $grid[$x+1][$y+2] + $grid[$x+2][$y+2]
-            if ($sum -gt $largestPower) {
-                $largestPower = $sum
-                $largestX = $x + 1
-                $largestY = $y + 1
-            }
-        }
-    }
-
-    write-host "$largestX,$largestY"
+Function Part2 {
+# FUCK THIS FUCKING SHIT
 }
 
-Part1
+Function CalculatePatch {
+    param(
+        [Parameter(Mandatory)]
+        [int[][]]
+        $grid,
+        [Parameter(Mandatory)]
+        [int]
+        $x,
+        [Parameter(Mandatory)]
+        [int]
+        $y,
+        [Parameter(Mandatory)]
+        [int]
+        $size
+    )
+    $sum = 0
+    for ($i = 0; $i -lt $size; $i++) {
+        for ($j = 0; $j -lt $size; $j++) {
+            $sum += $grid[$i][$j]
+        }
+    }
+    return $sum
+}
+
+#Part1
 Part2
